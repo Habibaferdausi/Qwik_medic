0d449a593e3a3f99ad1aa0805cf8b56fc47e8d0a
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-07-18 16:50:33 +0600

    add

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-purple-600 text-center text-3xl">
+        Welcome to Qwik Medic
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
